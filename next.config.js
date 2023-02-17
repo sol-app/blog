9b714077b95bbe93a53de7b8040642b219a0f930
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-17 22:43:22 +0330

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-module.exports = {
+/*module.exports = {
   reactStrictMode: true,
   i18n: {
     locales: ['en'],
     defaultLocale: 'en',
   },
+}*/
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  reactStrictMode: true,
 }
+
+module.exports = nextConfig
