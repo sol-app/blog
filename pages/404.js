1be461dba4de88f3928a016b0c47f20b345311e9
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-17 20:15:26 +0330

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,9 @@
 import Head from 'next/head';
 export default function Custom404() {
-  <Head>
+ return (
+   <Head>
     <title>Sol Blog</title>   
   </Head>
-  return <h1 className='text-center mx-auto w-full font-bold text-2xl '>404 - Page Not Found</h1>
+  <h1 className='text-center mx-auto w-full font-bold text-2xl '>404 - Page Not Found</h1>
+ )
 }
