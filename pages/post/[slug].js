a32a903b2d7c0066663a70135f5ac01798332675
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-17 18:12:18 +0330

    Update [slug].js

--- pages/post/[slug].js
+++ pages/post/[slug].js
@@ -45,12 +45,6 @@ export default function PostPage({ frontmatter, content }) {
             <span><b className='text-gray-500'>{frontmatter.title}</b></span>
         </nav>
 
-       <p class="bg-gray-300 p-4 rounded-lg mx-auto my-4 text-sm font-base">
-         use this blog for your project <br /> 
-         <pre class="bg-gray-700 text-white p-2 rounded-lg my-4"><code>
-           npx create-next-app@latest nextjs-blog --use-npm --example "https://github.com/sol-app/blog"
-          </code></pre>
-        </p>
 
       </div>
       
