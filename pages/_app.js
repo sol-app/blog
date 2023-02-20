f367bd811c6d262f7d36e3d0d93f6f8941baf81b
mosi <99150661+mosi-sol@users.noreply.github.com>
2023-02-21 00:51:15 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import Layout from '../components/layout';
 import '../styles/globals.css';
+import NextScript from 'next/script';
 
 function MyApp({ Component, pageProps }) {
   return (
+    
+    {process.env.NODE_ENV === 'production' && (
+        <>
+          <NextScript async src="https://www.googletagmanager.com/gtag/js?id=G-3HXER7XDKH"></NextScript>
+          <NextScript>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)};{'}'}
+            gtag('js', new Date());
+
+              gtag('config', 'G-3HXER7XDKH');
+            </NextScript>
+        </>
+      )}
+    
     <Layout>
       <Component {...pageProps} />
     </Layout>
